f9cbb2a6e3fba13ae22f7aea494174e66c0b649c
arehmandev <arehmandev@outlook.com>
2017-03-06 22:41:47 +0000

    Cleaned up bash

--- Scripts/delete-statebucket.sh
+++ Scripts/delete-statebucket.sh
@@ -4,7 +4,8 @@ BUCKET=$(cat Scripts/statebucket.txt)
 REGION=$(terraform output default_region)
 
 aws s3api delete-object --bucket ${BUCKET} --key terraform.tfvars
-aws s3api delete-bucket --bucket ${BUCKET} --region ${REGION}
-rm -rf Scripts/statebucket.txt
 
-echo "Successfully deleted remote tfstate bucket"
+aws s3api delete-bucket --bucket ${BUCKET} --region ${REGION} \
+&& echo "Successfully deleted remote tfstate bucket"
+
+rm -rf Scripts/statebucket.txt
