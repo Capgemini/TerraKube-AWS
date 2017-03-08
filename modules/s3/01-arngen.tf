4c4dc84637798845ed590b8d27ca8d5846779f50
arehmandev <arehmandev@outlook.com>
2017-03-08 18:17:35 +0000

    Cleaned up s3 module logic

--- modules/s3/01-arngen.tf
+++ modules/s3/01-arngen.tf
@@ -1,24 +1,12 @@
-data "template_file" "kubearn" {
-  template = "${file("${path.module}/Files/workerarn.sh")}"
-
-  vars {
-    worker-role = "${var.worker-role}"
-  }
-}
-
 data "aws_caller_identity" "current" {}
-
-output "account_id" {
-  value = "${data.aws_caller_identity.current.account_id}"
-}
 
 data "template_file" "kmspolicy" {
   depends_on = ["data.aws_caller_identity.current"]
   template   = "${file("${path.module}/Files/kmspolicy.json.tpl")}"
 
   vars {
-    arn       = "arn:aws:iam::${data.aws_caller_identity.current}:role/worker_role"
-    masterarn = "arn:aws:iam::${data.aws_caller_identity.current}:role/master_role"
-    rootarn   = "arn:aws:iam::${data.aws_caller_identity.current}:root"
+    arn       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/worker_role"
+    masterarn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/master_role"
+    rootarn   = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
   }
 }
