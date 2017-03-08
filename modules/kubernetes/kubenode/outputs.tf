feb66060fd08ce3908731d89c744182228dd15ce
arehmandev <arehmandev@outlook.com>
2017-03-08 21:36:10 +0000

    Added conditional placement group output

--- modules/kubernetes/kubenode/outputs.tf
+++ modules/kubernetes/kubenode/outputs.tf
@@ -9,5 +9,6 @@ output "launch_config_id" {
 
 // Output the ID of the Launch Config
 output "asg_id" {
+  value = "${var.placement_group == "true" ? aws_autoscaling_group.kubenode.id : aws_autoscaling_group.kubenode_noplacementgroup.id }"
   value = "${aws_autoscaling_group.kubenode.id}"
 }
