e825209efaa4357f3cd99c32cd7b31016de5bb8a
Abdul Rehman <arehmandev@outlook.com>
2017-07-21 22:08:43 +0100

    Added clarity on the notification

--- Scripts/cluster-test.sh
+++ Scripts/cluster-test.sh
@@ -11,7 +11,7 @@ _retry() {
 echo "❤ Polling for cluster life - this could take a minute or more"
 
 _retry "❤ Waiting for DNS to resolve for ${ELB}" ping -c1 "${ELB}"
-_retry "❤ Curling apiserver external elb" curl --insecure --silent "https://${ELB}"
+_retry "❤ Curling apiserver external elb (this takes a while)" curl --insecure --silent "https://${ELB}"
 sleep 15
 _retry "❤ Trying to connect to cluster with kubectl" kubectl cluster-info
 
